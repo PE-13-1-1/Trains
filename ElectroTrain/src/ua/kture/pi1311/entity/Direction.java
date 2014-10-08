5cbd69d321132cae1befe12498bb30568e21a8f2
Alfredovec <rud.sergey.v@gmail.com>
2014-10-08 22:23:53 +0300

    Добавлен перечислитель DirectionType

--- ElectroTrain/src/ua/kture/pi1311/entity/Direction.java
+++ ElectroTrain/src/ua/kture/pi1311/entity/Direction.java
@@ -1,10 +1,14 @@
 package ua.kture.pi1311.entity;
 
+import ua.kture.pi13.demo.enums.DirectionType;
+
 public class Direction {
 
 	private int directionId;
 	
 	private String directionTitle;
+	
+	private DirectionType directionType;
 	
 	public Direction() {
 	}
@@ -24,5 +28,13 @@ public class Direction {
 	public void setDirectionTitle(String directionTitle) {
 		this.directionTitle = directionTitle;
 	}
-
+	
+	public DirectionType getDirectionType() {
+		return this.directionType;
+	}
+	
+	public void setDirectionType(DirectionType directionType) {
+		this.directionType = directionType;
+	}
+		
 }
