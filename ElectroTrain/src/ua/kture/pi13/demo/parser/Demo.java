8e9488ab484aeea74c413c2dec020785d57a36e8
Женя <didevgen@gmail.com>
2014-10-21 00:25:32 +0300

    В парсере добился получения ссылок на каждую станцию

--- ElectroTrain/src/ua/kture/pi13/demo/parser/Demo.java
+++ ElectroTrain/src/ua/kture/pi13/demo/parser/Demo.java
@@ -1,28 +1,44 @@
 package ua.kture.pi13.demo.parser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
 
-
-
+import ua.kture.pi1311.entity.Train;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		Document doc;
 		try {
-			doc = Jsoup.connect("http://www.pz.gov.ua/prrasp/rasptrain.php?train=2212").get();
+			List<Train> trainList = new ArrayList<Train>();
+			List<String> set = new ArrayList<String>();
+			doc = Jsoup.connect("http://www.pz.gov.ua/prrasp/s_khar1.php")
+					.get();
 			Elements links = doc.getElementsByClass("bs1t");
 			for (Element link : links) {
-			  Elements elems = link.getElementsByTag("td");
-			  for (Element element : elems) {
-				  System.out.println(element.text());
-			  }
+				Elements elems = link.getElementsByTag("td");
+				Elements elems1 = link.getElementsByTag("tr");
+				for (Element element : elems1) {
+					String[] array = element.toString().split("\n");
+					for (String str : array) {
+						if (str.contains("onclick")) {
+							set.add(str);
+						}
+					}
+				}
 			}
-			
+			for (String element : set) {
+				
+				element.subSequence(
+						element.indexOf("rasptrain"),
+						element.indexOf("train")+20);
+			}
+
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
